2bf60068635aa79f09215488972956b0a77c98c9
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2017-11-28 18:48:36 -0500

    method to evaluate wogt loss (on gt trained model), also irrelevant experiment setting changes

--- running_scripts.py
+++ running_scripts.py
@@ -11,7 +11,7 @@ from evaluator import *
 
 def training_unet_simulated_w_gt():
     results_dir = '/nrs/saalfeld/heinrichl/results_keras/'
-    name = 'Unet_henormaltransposed_orig'
+    name = 'Unet_4-64-3'
     trainingfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/training.h5'
     validationfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/validation.h5'
 
@@ -19,7 +19,8 @@ def training_unet_simulated_w_gt():
     print("output results and logs to:", exp_path)
 
     m = IsoNet(4, (16, 64, 64), simulate=True, from_groundtruth=True)
-    m.unet_simple_spec(3, 32, 3)
+    m.unet_simple_spec(4, 64, 3, batchnorm=False)
+    #m.fullyconv_unet_simple_spec(3,32,2)
     m.training_scheme()
     m.compile(1e-04, 10)
     m.save_json(exp_path+'model_def_json.txt')
@@ -31,20 +32,83 @@ def training_unet_simulated_w_gt():
     e.run(exp_path+'validation150.h5', t.bs)
 
 
+def upper_bound_wo_gt_eval():
+    trainingfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/training.h5'
+    validationfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/validation.h5'
+    results_dir = '/nrs/saalfeld/heinrichl/results_keras/'
+    exp_path = results_dir + 'Unet_henormaltransposed_orig/'
+
+    m = IsoNet(4, (16,64,64), simulate=True, from_groundtruth=False)
+    m.unet_simple_spec(3, 32, 3)
+    m.training_scheme()
+    t = Trainer(m, exp_path, trainingfile, validationfile, cubic=False)
+
+    loss = []
+    diff_1_2_loss = []
+    diff_1_3_loss = []
+    diff_avg_1_loss = []
+    diff_avg_1_2_loss = []
+    diff_avg_1_3_loss = []
+    diff_gt_mean_squared = []
+
+    loss_val = []
+    diff_1_2_loss_val = []
+    diff_1_3_loss_val = []
+    diff_avg_1_loss_val = []
+    diff_avg_1_2_loss_val = []
+    diff_avg_1_3_loss_val = []
+    diff_gt_mean_squared_val = []
+    for epoch in range(10, 151, 10):
+        print("Epoch:", epoch)
+        m.load_weights(exp_path+'weights{0:}.h5'.format(epoch))
+        m.compile(1e-04, 10)
+        l, d12, d13, da, da12, da13, _, dgt, lv, d12v, d13v, dav, da12v, da13v, _, dgtv = t.evaluate(100)
+        loss.append(l)
+        diff_1_2_loss.append(d12)
+        diff_1_3_loss.append(d13)
+        diff_avg_1_loss.append(da)
+        diff_avg_1_2_loss.append(da12)
+        diff_avg_1_3_loss.append(da13)
+        diff_gt_mean_squared.append(dgt)
+        loss_val.append(lv)
+        diff_1_2_loss_val.append(d12v)
+        diff_1_3_loss_val.append(d13v)
+        diff_avg_1_loss_val.append(dav)
+        diff_avg_1_2_loss_val.append(da12v)
+        diff_avg_1_3_loss_val.append(da13v)
+        diff_gt_mean_squared_val.append(dgtv)
+    with open('upperbound.p', 'w') as f:
+        pickle.dump((loss, diff_1_2_loss, diff_1_3_loss, diff_avg_1_loss, diff_avg_1_2_loss, diff_avg_1_3_loss,
+                 diff_gt_mean_squared, loss_val, diff_1_2_loss_val, diff_1_3_loss_val, diff_avg_1_loss_val,
+                 diff_avg_1_2_loss_val, diff_avg_1_3_loss_val, diff_gt_mean_squared_val),
+                f)
+    #for k in range(360):
+    #for steps in range(30,361, 30):
+        #print(k)
+        #x, y =t.evaluate(1)
+        #train.append(x)
+        #val.append(y)
+        #print([np.mean([out[i] for out in train]) for i in range(len(x))], [np.std([out[i] for out in train]) for i in
+        #                                                                          range(len(x))])
+        #print([np.mean([out[i] for out in val]) for i in range(len(y))], [np.std([out[i] for out in val]) for i in
+        #                                                                         range(len(y))])
+
+
 def generate_evaluation():
     validationfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/validation.h5'
-    exp_path = '/nrs/saalfeld/heinrichl/results_keras/FSRCNN_deleteme/'
-    model = IsoNet(4, (16,64,64), simulate=True, from_groundtruth=True)
-    model.fsrcnn_spec(240,64,4)
+    exp_path = '/nrs/saalfeld/heinrichl/results_keras/Unet3-32-2_wogt_10cubic/finetuning_avg10weights_lrs1/'
+    model = IsoNet(4, (16, 64, 64), simulate=True, from_groundtruth=True)
+    model.unet_simple_spec(3,32,2)
     model.training_scheme()
-    model.load_weights(exp_path+'weights06.h5')
+    model.load_weights(exp_path+'weights20.h5')
     e = Evaluator(model, data_path=validationfile)
-    e.run(exp_path+'validation06.h5', 6)
+    e.run(exp_path+'validation20.h5', 6)
 
 
 def evaluate_prediction():
     groundtruthfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/validation.h5'
-    predictionfile = '/nrs/saalfeld/heinrichl/results_keras/FSRCNN_defaultinit/validation49.h5'
+    predictionfile = '/nrs/saalfeld/heinrichl/results_keras/Unet3-32-2_wogt_10cubic/finetuning_avg10weights_lrs1' \
+                     '/validation30.h5'
     gt_arr = h5py.File(groundtruthfile, 'r')['raw']
     pred_arr = h5py.File(predictionfile, 'r')['raw']
     gt_arr = utils.cut_to_sc(gt_arr, 4, 0)
@@ -101,38 +165,38 @@ def continue_fsrcnn_training():
 
 def continue_unet_training():
     results_dir = '/nrs/saalfeld/heinrichl/results_keras/'
-    name = 'Unet_orig_initfromold'
+    name = 'Unet3-32-2wogt_testwithnewinit/finetuning'
     trainingfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/training.h5'
     validationfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/validation.h5'
-
-    exp_path = utils.get_exppath(results_dir, exp_name=name)
+    exp_path = results_dir + name + '/'
+    #exp_path = utils.get_exppath(results_dir, exp_name=name)
     print("output results and logs to:", exp_path)
-    mod = IsoNet(4,(16,64,64),simulate=True, from_groundtruth=True)
-    mod.unet_simple_spec(3,32,3)
+    mod = IsoNet(4, (16, 64, 64), simulate=True, from_groundtruth=False)
+    mod.unet_simple_spec(3, 32, 2)
     mod.training_scheme()
     mod.compile(1e-04, 1)
-    mod.save_json(exp_path+'model_def_json.txt')
-    mod.load_weights('/nrs/saalfeld/heinrichl/results_keras/longUnet_nl3_nf32_nc3_3868b61_scheduler10'
-                     '/weights02_keras2format.h5')
+    #mod.save_json(exp_path+'model_def_json.txt')
+    mod.load_weights('/nrs/saalfeld/heinrichl/results_keras/Unet3-32-2wogt_testwithnewinit/finetuning/weights80.h5')
     t = Trainer(mod, exp_path, trainingfile, validationfile, cubic=False)
-    t.run(151, start_epoch=2)
+    t.run(151, start_epoch=80)
 
 
 def training_unet_simulated_wo_gt():
     results_dir ='/nrs/saalfeld/heinrichl/results_keras/'
-    name_cubic = 'Unetwogt_test_keras2'
+    name_cubic = 'Unet3-32-3_wogt_10cubic'
     trainingfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/training.h5'
     validationfile = '/nrs/saalfeld/heinrichl/SR-data/FIBSEM/downscaled/bigh5-16isozyx/validation.h5'
 
-    exp_path = utils.get_exppath(results_dir, exp_name=name_cubic)
-    finetune_exp_path = exp_path + 'finetuning/'
+    exp_path = utils.get_exppath(results_dir, exp_name=name_cubic)#results_dir+name_cubic+'/'
+    #'/nrs/saalfeld/heinrichl/results_keras/Unetwogt_testwithnewinit0007/'#
+    finetune_exp_path = exp_path + 'finetuning_avg10weight/'
     os.mkdir(finetune_exp_path)
     print("output results and logs to:", exp_path)
 
-    pretraining_epochs = 51
-    finetuning_epochs = 100
-    h = 4
-    w = 64
+    pretraining_epochs = 11
+    finetuning_epochs = 140
+    h = 3
+    w = 32
     d = 3
 
     m_cubic = IsoNet(4, (16, 64, 64), simulate=True, from_groundtruth=True)
@@ -151,15 +215,15 @@ def training_unet_simulated_wo_gt():
     m.unet_simple_spec(h, w, d)
     m.training_scheme()
     m.compile(1e-04, 10)
-    m.save_json(finetune_exp_path + 'pretrain_model_def_json.txt')
+    m.save_json(finetune_exp_path + 'model_def_json.txt')
     m.load_weights(exp_path+'weights{0:}.h5'.format(pretraining_epochs-1))
 
-    t = Trainer(m, finetune_exp_path)
+    t = Trainer(m, finetune_exp_path, trainingfile, validationfile)
     t.run(pretraining_epochs + finetuning_epochs, start_epoch=pretraining_epochs)
 
     m_cubic.load_weights(finetune_exp_path + 'weights{0:}.h5'.format(pretraining_epochs + finetuning_epochs - 1))
     e_finetuned = Evaluator(m_cubic, data_path=validationfile)
-    e_finetuned.run(finetune_exp_path + 'validation{0:}.h5'.format(pretraining_epochs + finetuning_epochs))
+    e_finetuned.run(finetune_exp_path + 'validation{0:}.h5'.format(pretraining_epochs + finetuning_epochs), t.bs)
 
 
 if __name__ == '__main__':
--- visualization.py
+++ visualization.py
@@ -2,6 +2,7 @@ import cPickle as pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import matplotlib.lines as mlines
 
 
 def get_loss_values(loss_file):
@@ -76,6 +77,94 @@ def plot_multiple_losses(list_of_loss_files, psnr=True, log=False, labels=None, 
         ticklabels.set_fontsize(20)
     plt.show()
 
+
+def plot_upper_bound_training():
+    def normalize(*args):
+        findmax = []
+        for a in args:
+            findmax += a
+        max_for_norm = np.max(findmax)
+
+        normalized = ()
+        for a in args:
+            normalized += ([val/max_for_norm for val in a],)
+        return normalized
+
+    with open('upperbound.p', 'r') as f:
+    #    loss_norm, diff_1_2_loss_norm, diff_1_3_loss_norm, diff_avg_1_loss_norm, diff_avg_1_2_loss_norm, \
+    #    diff_avg_1_3_loss_norm, diff_gt_mean_squared_norm, loss_val_norm, diff_1_2_loss_val_norm, \
+    #    diff_1_3_loss_val_norm, diff_avg_1_loss_val_norm, diff_avg_1_2_loss_val_norm, diff_avg_1_3_loss_val_norm, \
+    #    diff_gt_mean_squared_val_norm = pickle.load(f)
+         loss, diff_1_2_loss, diff_1_3_loss, diff_avg_1_loss, diff_avg_1_2_loss, diff_avg_1_3_loss, \
+         diff_gt_mean_squared, loss_val, diff_1_2_loss_val, diff_1_3_loss_val, diff_avg_1_loss_val, \
+         diff_avg_1_2_loss_val, diff_avg_1_3_loss_val, diff_gt_mean_squared_val = pickle.load(f)
+#
+    #loss_norm, loss_val_norm = normalize(loss, loss_val)
+    loss_norm, loss_val_norm = normalize(loss, loss_val)
+    diff_1_2_loss_norm, diff_1_3_loss_norm, diff_1_2_loss_val_norm, diff_1_3_loss_val_norm = normalize(
+        diff_1_2_loss, diff_1_3_loss, diff_1_2_loss_val, diff_1_3_loss_val)
+    #diff_1_2_loss_norm, diff_1_3_loss_norm = diff_1_2_loss, diff_1_3_loss
+    #diff_1_2_loss_norm, diff_1_2_loss_val_norm = normalize(diff_1_2_loss, diff_1_2_loss_val)
+    #diff_1_3_loss_norm, diff_1_3_loss_val_norm = normalize(diff_1_3_loss, diff_1_3_loss_val)
+
+    diff_avg_1_loss_norm, diff_avg_1_2_loss_norm, diff_avg_1_3_loss_norm, diff_avg_1_loss_val_norm, \
+    diff_avg_1_2_loss_val_norm, diff_avg_1_3_loss_val_norm = normalize(diff_avg_1_loss, diff_avg_1_2_loss,
+                                                                       diff_avg_1_3_loss, diff_avg_1_loss_val,
+                                                                       diff_avg_1_2_loss_val, diff_avg_1_3_loss_val)
+    diff_gt_mean_squared_norm, diff_gt_mean_squared_val_norm = normalize(diff_gt_mean_squared, diff_gt_mean_squared_val)
+
+    l1, = plt.plot(loss_norm, linestyle='-', color='orange', label='total loss')
+    #l2, = plt.plot(loss_val_norm, linestyle='--', color='orange')
+    l3, = plt.plot(diff_gt_mean_squared_norm, linestyle='-', color='coral', label='mse loss')
+    #l4, = plt.plot(diff_gt_mean_squared_val_norm, linestyle='--', color='coral')
+
+    l5, = plt.plot(np.array(diff_1_2_loss_norm)*1., linestyle='-', color='teal', label='1-2-loss')
+    #l6, = plt.plot(np.array(diff_1_2_loss_val_norm)*1., linestyle='--', color='teal')
+    l7, = plt.plot(np.array(diff_1_3_loss_norm)*1., linestyle='-', color='turquoise', label='1-3-loss')
+    #l8, = plt.plot(np.array(diff_1_3_loss_val_norm)*1., linestyle='--', color='turquoise')
+#
+    #l9, = plt.plot(np.array(diff_avg_1_loss_norm)*1., linestyle='-', color='orchid', label='avg 1')
+    #l10, = plt.plot(np.array(diff_avg_1_loss_val_norm)*1., linestyle='--', color='orchid')
+    #l11, = plt.plot(np.array(diff_avg_1_2_loss_norm)*1., linestyle='-', color='fuchsia', label='avg 12')
+    #l12, = plt.plot(np.array(diff_avg_1_2_loss_val_norm)*1., linestyle='--', color='fuchsia')
+    #l13, = plt.plot(np.array(diff_avg_1_3_loss_norm)*1., linestyle='-', color='violet', label='avg 13')
+    #l14, = plt.plot(np.array(diff_avg_1_3_loss_val_norm)*1., linestyle='--', color='violet')
+    plt.semilogy()
+    full_line = mlines.Line2D([], [], color='k', linestyle='-', label='train')
+    dot_line = mlines.Line2D([], [], color='k', linestyle='--', label='val')
+    # plt.legend(handles=[l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, full_line,
+    #            dot_line],
+    plt.legend(handles=[l1,  l3, l5,  l7, full_line,],
+               prop={'size': 18}, loc=3)
+    plt.show()
+
+def make_nn_and_bicubic_for_exp(png_file):
+    import scipy.ndimage
+    import scipy.misc
+    import utils
+    import os.path
+    exp_dir = os.path.dirname(png_file)
+    hr_img = scipy.ndimage.imread(png_file,flatten=True)
+    plt.imshow(255-hr_img, 'Greys')
+    plt.show()
+    down_img = utils.downscale_manually(hr_img, 4, 0)
+    plt.imshow(255-down_img, 'Greys')
+    plt.show()
+    print(os.path.join(os.path.dirname(png_file), 'down.png'))
+    scipy.misc.imsave(os.path.join(exp_dir, 'down.png'), down_img)
+    nn_img = np.repeat(down_img, 4, axis=0)
+    plt.imshow(255-nn_img, 'Greys')
+    plt.show()
+    scipy.misc.imsave(os.path.join(exp_dir, 'nn.png'), nn_img)
+    cubic_img = utils.cubic_up(down_img, 4, axis=0)
+    plt.imshow(255-cubic_img, 'Greys')
+    plt.show()
+    scipy.misc.imsave(os.path.join(exp_dir, 'cubic.png'), cubic_img)
+if __name__ == '__main__':
+    #make_nn_and_bicubic_for_exp('/groups/saalfeld/home/heinrichl/figures/wogt/exp4_100/gt.png')
+    plot_loss('/nrs/saalfeld/heinrichl/results_keras/Unet3-32-2_wogt/finetuning_normalweight/loss_history15.p',
+              psnr=False, log=True)
+    #plot_upper_bound_training()
 
 def make_nn_and_bicubic_for_exp(png_file):
     import scipy.ndimage
